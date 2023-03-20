34404e2f6479163a809138ab1552743c8523ce5c
dinhthuyhoa <dinhthuyhoa61@gmail.com>
2023-03-20 14:08:40 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,14 @@ import ContactBook from "@/views/ContactBook.vue";
 
 const routes = [
     {
-    path: "/",
-    name: "contactbook",
-    component: ContactBook,
+        path: "/",
+        name: "contactbook",
+        component: ContactBook,
+    },  
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
     },
 ];
 
